ded87a650bdded9a451a762334e343ae5ddde954
Maja Boldt <mboldt@databay.de>
2024-07-11 11:28:30 +0200

    adds first animation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,96 @@
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 import LEDStripe from './components/LEDStripe';
 
-
 function App() {
+  const animationRef = useRef();
+  const [animation, setAnimation] = useState("Start");
+
+  let id = 0;
+  let inc = 1;
+  const prefix = "led";
+  const diff = 100;
+  const millis = () => Date.now();
+  let last = millis();
+
+  function fx1() {
+
+    if (millis() - last >= diff) {
+      let currentId = prefix + (id);
+      let current = document.getElementById(currentId);
+      if (!current) {
+        inc *= -1;
+        id += 2 * inc;
+        currentId = prefix + (id);
+        current = document.getElementById(currentId);
+      }
+
+      const prevId = prefix + (id - inc);
+      const prev = document.getElementById(prevId);
+      if (prev) {
+        prev.style.backgroundColor = "red";
+      }
+
+      if (current) {
+        current.style.backgroundColor = "white";
+
+        console.log(current);
+        id += inc;
+      }
+
+      last = millis();
+    }
+
+  }
+
+  const myCallback = useCallback(function () {
+
+    fx1();
+
+
+    animationRef.current = requestAnimationFrame(myCallback);
+  });
+
+  useEffect(() => {
+    if (animation === "Stop") {
+      animationRef.current = requestAnimationFrame(myCallback);
+
+      return () => {
+        cancelAnimationFrame(animationRef.current);
+      }
+    } else if (animationRef.current) {
+      cancelAnimationFrame(animationRef.current);
+    }
+
+  }, [animation]);
+
+  function animate(event) {
+    if (event.target.innerText === "Start") {
+      setAnimation("Stop");
+    } else {
+      setAnimation("Start");
+    }
+  }
+
+
   return (
     <div className="App">
 
-      <LEDStripe count={30} radius={10} x={200} y={200}/>
+      <LEDStripe count={20} radius={20} x={200} y={200} prefix={"led"} />
 
       <header className="App-header">
-        
+
         DREAMLIGHTS
-        
+
       </header>
+
+      <button onClick={(e) => animate(e)}>{animation}</button>
     </div>
   );
+
+
+
 }
+
 
 export default App;
--- src/components/LEDStripe.js
+++ src/components/LEDStripe.js
@@ -1,6 +1,6 @@
 import { createLEDs } from "../functions";
 
-export default function LEDStripe({count, radius, x, y}) {
+export default function LEDStripe({count, radius, x, y, prefix}) {
     let currentLED = createLEDs(x, y, radius, count);
     let divToDrag = null;
 
@@ -10,14 +10,6 @@ export default function LEDStripe({count, radius, x, y}) {
     document.addEventListener('mouseup', function () {
         divToDrag = null;
     }, true);
-
-
-    return [...Array(count)].map((e, i) => {
-        const componentLED = <div className='led' key={i} id={"led" + i} style={{ left: currentLED.x, top: currentLED.y, width: currentLED.r * 2, height: currentLED.r * 2 }} onMouseDown={(e) => onMouseDown(e)} ></div>;
-        currentLED = currentLED.next;
-        return componentLED;
-    })
-
 
     function onMouseDown(e) {
         divToDrag = e.target;
@@ -41,13 +33,13 @@ export default function LEDStripe({count, radius, x, y}) {
     function moveLED(div, x, y) {
         div.style.left = x + "px";
         div.style.top = y + "px";
-        const id = parseInt(div.id.substring(3));
+        const id = parseInt(div.id.substring(prefix.length));
 
         if (id !== 0) {
-            moveLEDNextTo(document.getElementById("led" + (id - 1)), x, y, -1);
+            moveLEDNextTo(document.getElementById(prefix + (id - 1)), x, y, -1);
         }
 
-        const next = document.getElementById("led" + (id + 1));
+        const next = document.getElementById(prefix + (id + 1));
         if (next) {
             moveLEDNextTo(next, x, y, 1)
         }
@@ -56,8 +48,8 @@ export default function LEDStripe({count, radius, x, y}) {
     function moveLEDNextTo(div, x, y, diff) {
         const oldX = div.style.left;
         const oldY = div.style.top;
-        let posX = parseInt(oldX.substring(0, oldX.length -2)) - x;
-        let posY = parseInt(oldY.substring(0, oldY.length -2)) - y;
+        let posX = parseInt(oldX.substring(0, oldX.length - 2)) - x;
+        let posY = parseInt(oldY.substring(0, oldY.length - 2)) - y;
         const length = Math.sqrt(Math.pow(posX, 2) + Math.pow(posY, 2));
         const faktor = 2 * radius / length;
         posX = Math.floor(posX * faktor) + x;
@@ -67,13 +59,18 @@ export default function LEDStripe({count, radius, x, y}) {
         div.style.left = posX + "px";
         div.style.top = posY + "px";
 
-        const nextId = "led" + (parseInt(div.id.substring(3)) + diff);
+        const nextId = prefix + (parseInt(div.id.substring(prefix.length)) + diff);
         const next = document.getElementById(nextId);
         if (next) {
-            console.log(next)
             moveLEDNextTo(next, posX, posY, diff)
         }
     }
+
+    return [...Array(count)].map((e, i) => {
+        const componentLED = <div className='led' key={i} id={prefix + i} style={{ left: currentLED.x, top: currentLED.y, width: currentLED.r * 2, height: currentLED.r * 2 }} onMouseDown={(e) => onMouseDown(e)} ></div>;
+        currentLED = currentLED.next;
+        return componentLED;
+    })
 
 }
 
